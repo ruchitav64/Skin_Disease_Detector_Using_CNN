343cb9e868bfff7595c20474fc6df2f931fcb29b
Ruchita Verma <ruchi.v0604@gmail.com>
2026-02-05 17:48:27 +0530

    Optimized prediction logic

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import os
 import gdown
 from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
 from predict import predict_image
 
 app = Flask(__name__)
 
 MODEL_PATH = "skin_disease_model.h5"
 
+# Download model if not present
 if not os.path.exists(MODEL_PATH):
     print("Downloading model...")
     url = os.environ.get("MODEL_URL")
     gdown.download(url, MODEL_PATH, quiet=False)
 
-model= load_model(MODEL_PATH)
+# Load model
+model = load_model(MODEL_PATH)
 
 # Categories of diseases
 categories = ['eczema', 'melanoma', 'atopic_dermatitis', 'basal_cell_carcinoma', 'melanotic_nevi']
@@ -30,18 +33,16 @@ def predict():
     if 'file' not in request.files:
         return "No file uploaded", 400
     
-    file = request.files['file']  # This is a FileStorage object
+    file = request.files['file']
     
     if file.filename == '':
         return "No file selected", 400
     
-    result = predict_image("skin_disease_model.h5", file, categories=categories)
+    # Pass MODEL instead of path
+    result = predict_image(model, file, categories=categories)
+    
     return render_template('detect.html', prediction=result)
-
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
--- predict.py
+++ predict.py
@@ -1,26 +1,22 @@
 import io
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-from tensorflow.keras.models import load_model
 
-def predict_image(model_path, file, target_size=(128, 128), categories=None):
-    # Load the model
-    model = load_model(model_path)
+def predict_image(model, file, target_size=(128, 128), categories=None):
     
     # Convert FileStorage to BytesIO
-    img_bytes = file.read()  # Read the file content
-    img = io.BytesIO(img_bytes)  # Convert to BytesIO object
+    img_bytes = file.read()
+    img = io.BytesIO(img_bytes)
     
-    # Load image from the byte stream
-    image = load_img(img, target_size=target_size)  # Use the BytesIO stream here
+    # Load image
+    image = load_img(img, target_size=target_size)
     
-    # Preprocess the image
-    image_array = img_to_array(image) / 255.0  # Normalize pixel values
-    image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
+    # Preprocess image
+    image_array = img_to_array(image) / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
     
-    # Make prediction
+    # Prediction
     prediction = model.predict(image_array)
     predicted_class = np.argmax(prediction)
     
-    # Return the predicted category
     return categories[predicted_class] if categories else predicted_class
